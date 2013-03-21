0c6ea022cce840f88e7679fd04749cf89c4a2309
Jakub Sygnowski <js319394@khaki13.mimuw.edu.pl>
2013-03-21 11:19:05 +0100

    drobna zmiana

--- communication.py
+++ communication.py
@@ -20,7 +20,7 @@ class TestServerCommunication(unittest.TestCase):
             self.th.start()
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             sock.sendto('hello', ('127.0.0.1', 10034))
-            #nie wiedziec czemu, jesli nie zdazy dostac tej wiadomosci, to sie wiesza?
+# nie wiedziec czemu, jesli nie zdazy dostac tej wiadomosci, to sie wiesza?
             sleep(0.1)
             self.assertEqual(self.th.a.buf, "hello")
             sock.sendto('kill server', ('127.0.0.1', 10034))

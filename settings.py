8121e9de3955fc968b4c883ae112766d133250df
Janek <gumowyjohn@gmail.com>
2013-04-06 12:28:45 +0200

    Added drawing of example game screen in settings.py.

--- settings.py
+++ settings.py
@@ -1,9 +1,57 @@
+"""
+board example
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                                                                                                                        X
+     Your HP: 18                                                                              Enemy HP: 22              X
+          CPS: 4.11                                                                                 CPS: 5.8            X
+          typo rate: 0.01                                                                           typo rate: 0.0      X
+________________________________________________________________________________________________________________________X
+               star||                                                                                            ||     X
+              candy||                                                                                            ||     X
+              wings||                                                                                            ||     X
+        salutations||                                                                                            ||     X
+              power||                                                                                            ||     X
+             string||                                                                                            ||     X
+         carburetor||                                                                                            ||     X
+           shopping||                                                                                            ||     X
+              blond||                                                                                            ||     X
+              steak||                                                                                            ||     X
+           speakers||                                                                                            ||     X
+            grimace||                                                                                            ||     X
+               case||                                                                                            ||     X
+           stubborn||                                                                                            ||     X
+              couch||                                                                                            ||     X
+       announcement||                                                                                            ||     X
+                cat||                                                                                            ||     X
+           elevator||                                                                                            ||     X
+             marker||                                                                                            ||     X
+              swish||                                                                                            ||     X
+           gangrene||                                                                                            ||     X
+         scurrilous||                                                                                            ||     X
+             photon||                                                                                            ||     X
+              cabal||                                                                                            ||     X
+            stentor||                                                                                            ||     X
+           youngish||                                                                                            ||     X
+              swish||                                                                                            ||     X
+              elver||                                                                                            ||     X
+            organza||                                                                                            ||     X
+            febrile||                                                                                            ||     X
+‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾X
+                                                                                                                        X
+                    DEBUG                                                                                               X
+                                                                                                                        X
+                                                                                                                        X
+                                                                                                                        X
+                                                                                                                        X
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+"""
 import curses
 
 DEFAULT_BACKGROUND_COLOR = curses.COLOR_BLACK
 TYPO_FLASH_COLOR = curses.COLOR_RED
 OUR_BULLETS_COLOR = curses.COLOR_CYAN
 ENEMY_BULLETS_COLOR = curses.COLOR_BLUE
+AFTER_DAMAGE_BACKGROUND_COLOR = curses.COLOR_MAGENTA
 
 DISPLAY_WIDTH = 120
 DISPLAY_HEIGHT = 50
